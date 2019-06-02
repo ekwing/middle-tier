55005a09c0eb44398a5cf963fc549fad5555ed3f
L-Chris <563303226@qq.com>
2019-06-02 14:59:21 +0800

    update

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+const router = require('koa-router')()
+
+router.use('/')
+
+module.exports = router
---
+++ src/services/base.service.js
@@ -0,0 +1,7 @@
+import axios from './create-axios'
+
+class BaseService {}
+
+export {
+  BaseService
+}
