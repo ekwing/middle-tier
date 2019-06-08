1f6143312c3ca803563d0ddbc82875cb7346c45a
L-Chris <563303226@qq.com>
2019-06-08 23:04:44 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
 const Koa = require('koa')
-const setBodyParser = require('koa-bodyparser')
-const setJsonPrinter = require('koa-json')
-const setLogger = require('koa-logger')
-const setViews = require('koa-views')
+const bodyParser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const views = require('koa-views')
 const router = require('./routes')
 
 const app = new Koa()
 
-app.use(setBodyParser({
+app.use(bodyParser({
   enableTypes: ['json', 'form', 'text']
 }))
 
-app.use(setJsonPrinter())
+app.use(logger())
 
-app.use(setLogger())
-
-app.use(setViews(`${__dirname}/views`, {
+app.use(views(`${__dirname}/views`, {
   extension: 'pug'
 }))
 
